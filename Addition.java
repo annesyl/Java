6a1895d236297c30013435459363b697243809ec
annesyl <anne_joseph2008@yahoo.com>
2016-03-22 23:36:54 -0400

    Added files via upload

---
+++ Addition.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+public class Addition{
+
+	public void Addition(){}
+	public static int add(){
+	int n1=10;
+	int n2=20;
+	int c;
+	c=n1+n2;
+	return c;
+}
+}
---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class HelloWorld
+{
+  public static void main(String[] args)
+  {
+    //System.out.println("Hello, World!");
+	Addition a=new Addition();
+	int result = a.add();
+	System.out.println("Sum is :" +result);
+  }
+
+	
+}
